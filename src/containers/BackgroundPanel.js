be6ac1f713778889ab3e8c572401d745fa1be952
zhangsh <zsh528939425@163.com>
2019-08-15 20:08:44 +0800

    背景色加载动画

--- src/containers/BackgroundPanel.js
+++ src/containers/BackgroundPanel.js
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import { getBackgroundColorEnum } from '../apis'
+import { Spin } from 'antd'
 import ColorPicker from '../components/ColorPicker'
 import styles from './BackgroundPanel.module.less'
 
 const BackgroundPanel = ({ onSelect }) => {
+  const [loading, toggleLoading] = useState(false)
   const [backgroundColors, setBackgroundColors] = useState([])
 
   useEffect(() => {
-    getBackgroundColorEnum().then(setBackgroundColors)
+    toggleLoading(true)
+    getBackgroundColorEnum()
+      .then(setBackgroundColors)
+      .then(() => toggleLoading(false))
   }, [])
 
   return (
     <div className={styles.container}>
       <header className={styles.header}>背景色</header>
-      <ColorPicker colorEnum={backgroundColors} onSelect={onSelect} />
+      <Spin spinning={loading}>
+        <ColorPicker colorEnum={backgroundColors} onSelect={onSelect} />
+      </Spin>
     </div>
   )
 }
